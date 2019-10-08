5691f5e2c7b19061d507d23bdee11464365e3842
金城理奈 <dontako0126@gmail.com>
2019-10-08 21:27:21 +0900

    　中間制作のUPです

---
+++ custom.js
@@ -0,0 +1,16 @@
+
+$(function(){
+
+$('#login').click(function(){
+    $('#login_form').addClass('open');
+    $('.carousel-item').addClass('open_form');
+
+});
+    
+    
+//$('.running_cat').click(function(){
+//    $(this).addClass('animation-target');
+//});
+    
+     
+});
